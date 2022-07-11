33efbb0f94e997b29085929e2e897df428fca32b
AgusPriatna <86345766+Agus-Priatna@users.noreply.github.com>
2022-07-11 12:46:58 +0800

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+const APP_TOKEN = '69570ed75d1ea7d554ad2af809d69bf1f722857d45f923e3011e39f8addb6435'
+const REQUEST_HEADER = {
+    Accept: 'application/json',
+    'Content-Type' : 'application/json',
+    Authorization: `Bearer ${APP_TOKEN}`
+}
+
+export { REQUEST_HEADER }
